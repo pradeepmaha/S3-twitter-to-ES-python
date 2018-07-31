b2963341146092864b5a7b10e45da5a15f350f48
pradeep <pradeep.mahalingam@reancloud.com>
2018-07-31 16:43:33 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+'''
+Created on Oct 12, 2015
+
+@author: mentzera
+
+'''
+
+es_host = 'search-es-twitter-queo37vkepjzrotyfq5cmle2cu.us-east-2.es.amazonaws.com' #without the https - for example: search-es-twitter-demo-xxxxxxxxxxxxxxxxxxx.us-east-1-.es.amazonaws.com
+es_port = 80
+es_bulk_chunk_size = 1000  #number of documents to index in a single bulk operation
+
